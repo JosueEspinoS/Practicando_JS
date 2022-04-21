deb51f14823b3796af5caca011451baf6b0b45e9
JosueEspinoS <josue.espino.soto.git@gmail.com>
2022-04-20 19:57:00 -0500

    Wheel Decide, Acceder a un elemento de Array aleatoriamente

---
+++ wheelDecide.js
@@ -0,0 +1,16 @@
+console.log('Wheel Decide')
+
+var list = ["BM", "RED", "Blue", "Cobra", "Dragon"]
+
+var results = {
+    category: list[Math.floor(Math.random() * list.length)],
+    //second: list[Math.floor(Math.random() * list.length)],
+    //third: list[Math.floor(Math.random() * list.length)]
+
+}
+
+console.log(results);
+
+//console.log(list[Math.floor(Math.random() * list.length)])
+
+//console.log(Math.floor(Math.random() * list.length))
